6fbb62fa613992ec753d3a23bf26fef355726491
= <=>
2020-12-29 21:47:31 +0630

    add @Inheritance annotation in Employee class

--- hibernate-table-per-hierarchy-mapping-using-annotation/src/main/java/me/khun/hibernate/entity/Employee.java
+++ hibernate-table-per-hierarchy-mapping-using-annotation/src/main/java/me/khun/hibernate/entity/Employee.java
@@ -6,6 +6,7 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "employee")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "type", discriminatorType = DiscriminatorType.STRING)
 @DiscriminatorValue(value = "employee")
 @Getter
